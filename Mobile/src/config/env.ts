08f8b8fa1c244884d1cb628daed532eb11971df7
elizabeth <elizabeth@nodex.lan>
2026-04-04 15:18:26 -0600

    Mobile nuevo

--- Mobile/src/config/env.ts
+++ Mobile/src/config/env.ts
@@ -1,8 +1,18 @@
 import Constants from 'expo-constants';
 import { Platform } from 'react-native';
 
+function getExpoHostApiBaseUrl() {
+  const hostUri = Constants.expoConfig?.hostUri;
+  if (!hostUri) return undefined;
+
+  const host = hostUri.split(':')[0];
+  if (!host) return undefined;
+
+  return `http://${host}:3000/api`;
+}
+
 const DEFAULT_API_BASE_URL = Platform.OS === 'android'
-  ? 'http://10.0.2.2:3000/api'
+  ? (getExpoHostApiBaseUrl() || 'http://10.0.2.2:3000/api')
   : 'http://localhost:3000/api';
 
 function normalizeApiBaseUrl(url: string | undefined) {
